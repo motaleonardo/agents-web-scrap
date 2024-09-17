7fc4f288c1173a74b6b2ecc64c1f79f4e7230b72
motaleonardo <mota.vleonardo@gmail.com>
2024-09-17 19:27:15 -0300

    first comit

---
+++ testeAPI.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+url = 'http://127.0.0.1/reasearch_candidates'
+headers = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json',
+}
+data = {
+    'job_requirements': 'Data Engineer & Data Architect - Databricks Professional Certified - brasileiro que resida em Goiânia.'
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+print(f"Status Code: {response.status_code}")
+print("Response JSON:")
+print(json.dumps(response.json(), indent=4, ensure_ascii=False))
